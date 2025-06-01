c3dc2892e9dd39dcd5d3f7ac93ff14528d27a9a3
lucaapacitti <ljmp201@exeter.ac.uk>
2025-06-01 19:00:15 +0000

    Set up Product class with constructor and get/set methods

--- src/listgenerator/Product.java
+++ src/listgenerator/Product.java
@@ -1,1 +1,64 @@
+package listgenerator;
 
+public class Product
+{
+    private int ID;
+    private String name;
+    private String category;
+    private double price;
+    private int location;
+
+    public Product(int ID, String name, String category, double price, int location)
+    {
+        this.ID = ID;
+        this.name = name;
+        this.category = category;
+        this.price = price;
+        this.location = location;
+    }
+
+    public int getID()
+    {
+        return ID;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public String getCategory()
+    {
+        return category;
+    }
+
+    public void setCategory(String category)
+    {
+        this.category = category;
+    }
+
+    public double getPrice()
+    {
+        return price;
+    }
+
+    public void setPrice(double price)
+    {
+        this.price = price;
+    }
+
+    public int getLocation()
+    {
+        return location;
+    }
+
+    public void setLocation(int location)
+    {
+        this.location = location;
+    }
+}
