42213c748f81dda2c5c565f4774b0798dd2f7c21
lucaapacitti <ljmp201@exeter.ac.uk>
2025-06-01 19:09:27 +0000

    Set up List class with constructor and get/set methods

--- src/listgenerator/List.java
+++ src/listgenerator/List.java
@@ -1,1 +1,47 @@
+package listgenerator;
 
+public class List
+{
+    private int ID;
+    private String name;
+    private User user;
+    private ArrayList<Product> items;
+
+    public List(int ID, String name, User user)
+    {
+        this.ID = ID;
+        this.name = name;
+        this.user = user;
+        items = new ArrayList<Product>();
+    }
+
+    public int getID()
+    {
+        return ID;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public User getUser()
+    {
+        return user;
+    }
+
+    public ArrayList<Product> getItems()
+    {
+        return items;
+    }
+
+    public void setItems(ArrayList<Product> items)
+    {
+        this.items = items;
+    }
+}
